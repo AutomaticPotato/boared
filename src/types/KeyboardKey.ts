b46ad4d5c3f9a74b3427c45fd69028c2e2d5029e
Jeroen Jonker <jeroen.jonker@capteur.nl>
2024-03-27 12:07:58 +0100

    Added is back

--- src/types/KeyboardKey.ts
+++
@@ -1,8 +0,0 @@
-import type { KeyboardKeyUnits } from '@/enums/KeyboardKeyUnits'
-
-export type KeyboardKey = {
-  primarySymbol: string
-  secondarySymbol?: string
-  unit: KeyboardKeyUnits
-  name?: string
-}
