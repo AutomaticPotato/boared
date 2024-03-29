a2fdbb65db11cd3569cf82cded61fcbe1f7e7fa4
Jeroen Jonker <jeroen.jonker@capteur.nl>
2024-03-29 11:35:00 +0100

    Changed switch button positions

--- src/components/OptionsBar.vue
+++ src/components/OptionsBar.vue
@@ -10,11 +10,11 @@ const { playSound, stickyKeys, flyingKeys } = useOptions()
 <template>
     <div class="flex flex-wrap w-full pb-3 mb-5 border-b border-slate-300 gap-5">
         <SwitchButton v-model="playSound"> Play sound </SwitchButton>
+        <SwitchButton v-model="flyingKeys"> Flying keys </SwitchButton>
         <SwitchButton v-model="stickyKeys"> Sticky keys </SwitchButton>
         <Transition class="transition-all duration-300 ease-out" enter-from-class="scale-0" enter-to-class="scale-100"
             leave-to-class="scale-0">
             <ResetIcon v-if="stickyKeys" class="flex gap-2 items-center" />
         </Transition>
-        <SwitchButton v-model="flyingKeys"> Flying keys </SwitchButton>
     </div>
 </template>
